8151cfb53cd628165661ced981260289cf67073d
Henrique Eduardo Martins da Silva Filho <98719897+Henrique2301@users.noreply.github.com>
2024-05-25 13:45:33 -0300

    Update modal_add_evento.dart
    Atualização do modal evento

--- lib/widgets/modal_add_evento.dart
+++ lib/widgets/modal_add_evento.dart
@@ -2,106 +2,123 @@ import 'package:event_app/controllers/controller_eventos.dart';
 import 'package:event_app/models/evento_model.dart';
 import 'package:event_app/widgets/text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
-void modalNewEvento(BuildContext context) {
-  final evento = EventoModel();
+void modalNewEvento(BuildContext context, EventoModel? eventoModel) {
+  // Inicialize os valores do evento fora da árvore de widgets
+  final EventoModel evento = eventoModel != null
+      ? EventoModel(
+          id: eventoModel.id,
+          nomeEvento: eventoModel.nomeEvento,
+          descricaoEvento: eventoModel.descricaoEvento,
+          localEvento: eventoModel.localEvento,
+          dataEvento: eventoModel.dataEvento ?? DateTime.now(),
+        )
+      : EventoModel();
+
   showModalBottomSheet<void>(
     context: context,
     builder: (BuildContext context) {
-      final listaEventos = Provider.of<ControllerEventos>(context);
-      // final evento = Provider.of<EventoModel>(context);
-      evento.reset();
-      return SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          color: Colors.black,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
+      final listaEventos = Provider.of<ControllerEventos>(context, listen: false);
+
+      return ChangeNotifierProvider.value(
+        value: evento,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
                   padding: const EdgeInsets.symmetric(vertical: 10),
                   child: SizedBox(
                     width: 60,
                     height: 60,
                     child: Image.asset('assets/evento.png'),
-                  )),
-              
-              const Center(child: Padding(padding: EdgeInsets.only(top: 5, bottom: 20), child: Text('Novo evento', style: TextStyle(color: Colors.white),))),
-              Observer(builder: (_) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: textField(
+                  ),
+                ),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 20),
+                    child: Text('Novo evento', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+                Consumer<EventoModel>(builder: (context, meuModel, child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: textField(
                       labelText: "Nome do Evento",
-                      onChanged: evento.setNome,
-                      value: evento.nomeEvento,
-                      errorText: () => ''),
-                );
-              }),
-              Observer(builder: (_) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 30.0, vertical: 10),
-                  child: textField(
+                      onChanged: meuModel.setNome,
+                      value: meuModel.nomeEvento,
+                      errorText: () => '',
+                    ),
+                  );
+                }),
+                Consumer<EventoModel>(builder: (context, meuModel, child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+                    child: textField(
                       labelText: "Descrição do evento",
-                      onChanged: evento.setDescricao,
-                      value: evento.descricaoEvento,
-                      errorText: () => ''),
-                );
-              }),
-              Observer(builder: (_) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: textField(
+                      onChanged: meuModel.setDescricao,
+                      value: meuModel.descricaoEvento,
+                      errorText: () => '',
+                    ),
+                  );
+                }),
+                Consumer<EventoModel>(builder: (context, meuModel, child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: textField(
                       labelText: "Local do Evento",
-                      onChanged: evento.setLocal,
-                      value: evento.localEvento,
-                      errorText: () => ''),
-                );
-              }),
-              Observer(builder: (_) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                  child: ElevatedButton(
-                    onPressed:  () async {
-                     evento.setData(await showDatePicker(
-                            context: context,
-                            initialDate: evento.dataEvento,
-                            firstDate: DateTime(1950),
-                            lastDate: DateTime(2100)));                        
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      side: const BorderSide(color: Colors.orange, width: 2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                      onChanged: meuModel.setLocal,
+                      value: meuModel.localEvento,
+                      errorText: () => '',
+                    ),
+                  );
+                }),
+                Consumer<EventoModel>(builder: (context, meuModel, child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: meuModel.dataEvento ?? DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2100),
+                        );
+                        if (pickedDate != null) {
+                          meuModel.setData(pickedDate);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        side: const BorderSide(color: Colors.orange, width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Text(
+                        'Data do Evento ${meuModel.formatarData()}',
+                        style: const TextStyle(color: Colors.orange),
                       ),
                     ),
-                    child: Text(
-                      'Data do Evento ${evento.formatarData()}',
-                      style: const TextStyle(
-                          color: Colors
-                              .orange),
-                    ),
-                  ),
-                );
-              }),
-              // const SizedBox(height: 20),
-
-              Padding(
+                  );
+                }),
+                Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30.0),
                   child: ElevatedButton(
                     onPressed: () {
-                      evento.setId(null);
-                      listaEventos.add(evento);
+                      listaEventos.upsert(evento);
                       Navigator.pop(context);
                     },
                     child: const Text('Cadastrar'),
-                  )),
-            ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
